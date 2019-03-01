601712784d42b10d5b696db50c18f9a2c2f5bf87
Ishneet Singh Arora <iarora@iu.edu>
2019-03-01 05:51:17 +0000

    Automating Email Service

---
+++ deploy/email/deploy.sh
@@ -0,0 +1,14 @@
+sudo apt update;
+sudo apt install openjdk-8-jdk;
+sudo apt install maven;
+sudo apt install nodejs;
+sudo apt install npm;
+sudo npm install -g forever;
+cd ../../EmailService
+echo "Starting Service"
+npm install;
+npm install express;
+npm install express-handlebars ;
+npm install nodemailer;
+npm install cors;
+node app.js;
