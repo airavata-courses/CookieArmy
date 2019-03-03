61e6f4f7ab9e27e51fb7abbd4aaf40c9eedeba92
iarora93 <46791010+iarora93@users.noreply.github.com>
2019-03-02 19:38:22 -0500

    Update SgaMysqlFinalApplication.java

--- DbService/src/main/java/com/example/demo/SgaMysqlFinalApplication.java
+++ DbService/src/main/java/com/example/demo/SgaMysqlFinalApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SgaMysqlFinalApplication {
@@ -10,6 +13,18 @@ public class SgaMysqlFinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SgaMysqlFinalApplication.class, args);
 	}
-
+	
+	
+	    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/offering").allowedOrigins(""http://149.165.157.87:8082"");
+            }
+        };
+    }
+	
+	
 }
 
