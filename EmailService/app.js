6537f022d526a1e4d060313fcfe835845c78e7ba
iarora93 <46791010+iarora93@users.noreply.github.com>
2019-03-04 16:50:35 -0500

    Update app.js

--- EmailService/app.js
+++ EmailService/app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
+    res.send('Hello, Welcome to CookieArmy Email Service');
     //res.send('Hello');
     //   res.render('contact');
 
