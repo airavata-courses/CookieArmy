6d149f0a7e9191463edd848bb1fead6297565a52
RG <33567937+gattur@users.noreply.github.com>
2019-03-08 02:09:44 +0530

    Update run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,7 +3,7 @@ cd ../DbService;
 mvn clean install
 cd target
 echo "RUNNING APPLICATION NOW"
-
+ 
 #java -jar SGA-mysql-FINAL-0.0.1-SNAPSHOT.jar
 BUILD_ID=dontKillMe nohup java -jar SGA-mysql-FINAL-0.0.1-SNAPSHOT.jar &
 #java -jar /home/ubuntu/sga/jenkins/workspace/DB_Service/SGA-mysql-FINAL/target/SGA-mysql-FINAL-0.0.1-SNAPSHOT.jar
