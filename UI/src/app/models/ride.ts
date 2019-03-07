b622bfd65afc728af18cc1ce139815feabeaf80d
Ishneet Singh Arora <iarora@iu.edu>
2019-03-07 17:57:18 +0000

    Updated Email branch

--- UI/src/app/models/ride.ts
+++
@@ -1,7 +0,0 @@
-export class Ride{
-  
-    constructor(public rideid:string,public date:string,public source:string,public destination:string,public time:string,public name:string,public amount:number){}
-
-
-}
-
