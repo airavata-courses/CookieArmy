40174f0ebd4a9419e1e02eb353ddf54453a0d683
Ishneet Singh Arora <iarora@iu.edu>
2019-04-11 17:43:25 +0000

    Removed Unnecssary folders

--- UI/src/app/models/addride.ts
+++
@@ -1,8 +0,0 @@
-import { Ride } from "./ride";
-
-export class AddRide{
-    constructor(public rideid:string,public date:string,public source:string,public destination:string,public time:string,
-        public name:string,public amount:number,public contact_details:string){}
-
-        
-}
